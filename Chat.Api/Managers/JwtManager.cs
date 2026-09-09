58daed16fe222ecae638d22b7bc16dade3d8eb3d
Zulfiqor0912 <zulfiqorrustamov81@gmail.com>
2026-09-09 10:20:23 +0500

    as

--- Chat.Api/Managers/JwtManager.cs
+++ Chat.Api/Managers/JwtManager.cs
@@ -1,5 +1,7 @@
 ﻿using Chat.Api.Entities;
 using Chat.Api.Helpers;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 
 namespace Chat.Api.Managers;
@@ -16,11 +18,22 @@ public class JwtManager
     public string GenerateToken(User user)
     {
         var key = System.Text.Encoding.UTF32.GetBytes(jwtParameters.Key);
+        var signingKey = new SigningCredentials(new SymmetricSecurityKey(key), "Hs256");
 
         var claims = new List<Claim>()
         {
             new Claim(ClaimTypes.Name, user.Username),
-            new Claim(ClaimTypes)
-        }
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+        };
+
+        var security = new JwtSecurityToken(
+            issuer: jwtParameters.Issuer, 
+            audience: jwtParameters.Audience, 
+            signingCredentials: signingKey, 
+            claims: claims);
+
+        var token = new JwtSecurityTokenHandler()
+            .WriteToken(security);
+        return token;
     }
 }
